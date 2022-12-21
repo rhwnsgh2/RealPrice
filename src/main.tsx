ae38997ba3e34750a5c32570c7f262e1b2a34902
junho <junho.ko@business-canvas.com>
2022-12-21 20:43:12 +0900

    add extension

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const extensionRoot = document.createElement('div')
+extensionRoot.id = "realPriceExtension"
+document.body.appendChild(extensionRoot)
+console.log("TESTSTETSETES")
+ReactDOM.createRoot(document.getElementById('realPriceExtension') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
