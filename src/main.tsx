856ebdc57bd3fb39aeb801d430349ea38873838d
junho <junho.ko@business-canvas.com>
2022-12-22 14:23:39 +0900

    adjust eslint

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-const extensionRoot = document.createElement('div')
-extensionRoot.id = "realPriceExtension"
-document.body.appendChild(extensionRoot)
+const extensionRoot = document.createElement("div");
+extensionRoot.id = "realPriceExtension";
+document.body.appendChild(extensionRoot);
 
-ReactDOM.createRoot(document.getElementById('realPriceExtension') as HTMLElement).render(
+ReactDOM.createRoot(
+  document.getElementById("realPriceExtension") as HTMLElement
+).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
