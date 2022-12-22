e4a82aa7414747c9623577b92fdd3cf9dce97e85
junho <junho.ko@business-canvas.com>
2022-12-22 14:20:18 +0900

    add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-      안녕하세요 
-    </div>
-  )
+  return <div>안녕하세요</div>;
 }
 
-
-export default App
+export default App;
