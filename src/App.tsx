c982e083fcfcb61054c3b919af34a653bb4400c2
junho <junho.ko@business-canvas.com>
2022-12-26 19:41:02 +0900

    add Root div

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import styled from "@emotion/styled";
 import "./App.css";
 
 function App() {
-  return <div>안녕하세요</div>;
+  return <Root>안녕하세요</Root>;
 }
 
 export default App;
+
+const Root = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  height: 100%;
+  z-index: 2147483647;
+`;
